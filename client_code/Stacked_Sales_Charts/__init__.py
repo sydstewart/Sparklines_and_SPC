8b10b8c2124a01390e0947af0fd3de5ca3a78015
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2022-02-11 12:52:54 +0000

    Edited Stacked_Sales_Charts

--- client_code/Stacked_Sales_Charts/__init__.py
+++ client_code/Stacked_Sales_Charts/__init__.py
@@ -13,69 +13,78 @@ class Stacked_Sales_Charts(Stacked_Sales_ChartsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    chartid = 88
+    
+    chartid = 106
     chart_copy = app_tables.charts.get(id = chartid) 
     print(chart_copy['id'])
     chart_position = 1
     create_step_chart(self,chart_copy, chart_position)
     
-    chartid = 99
+        
+    
+    chartid = 88
     chart_copy = app_tables.charts.get(id = chartid) 
     print(chart_copy['id'])
     chart_position = 2
     create_step_chart(self,chart_copy, chart_position)
     
-    chartid = 100
+    chartid = 99
     chart_copy = app_tables.charts.get(id = chartid) 
     print(chart_copy['id'])
-    chart_position = 3
+    chart_position =3
     create_step_chart(self,chart_copy, chart_position)
     
-    chartid = 50
+    chartid = 100
     chart_copy = app_tables.charts.get(id = chartid) 
     print(chart_copy['id'])
     chart_position = 4
     create_step_chart(self,chart_copy, chart_position)
     
-    chartid = 96
+    chartid = 50
     chart_copy = app_tables.charts.get(id = chartid) 
     print(chart_copy['id'])
     chart_position = 5
     create_step_chart(self,chart_copy, chart_position)
-    # Any code you write here will run when the form opens.
     
-    chartid = 71
+    chartid = 96
     chart_copy = app_tables.charts.get(id = chartid) 
     print(chart_copy['id'])
     chart_position = 6
     create_step_chart(self,chart_copy, chart_position)
     # Any code you write here will run when the form opens.
     
-    chartid = 61
+    chartid = 71
     chart_copy = app_tables.charts.get(id = chartid) 
     print(chart_copy['id'])
     chart_position = 7
     create_step_chart(self,chart_copy, chart_position)
     # Any code you write here will run when the form opens.
     
-    chartid = 52
+    chartid = 61
     chart_copy = app_tables.charts.get(id = chartid) 
     print(chart_copy['id'])
     chart_position = 8
     create_step_chart(self,chart_copy, chart_position)
     # Any code you write here will run when the form opens.
     
-    chartid = 53
+    chartid = 52
     chart_copy = app_tables.charts.get(id = chartid) 
     print(chart_copy['id'])
     chart_position = 9
     create_step_chart(self,chart_copy, chart_position)
     # Any code you write here will run when the form opens.
     
-    chartid = 97
+    chartid = 53
     chart_copy = app_tables.charts.get(id = chartid) 
     print(chart_copy['id'])
     chart_position = 10
+    create_step_chart(self,chart_copy, chart_position)
+    # Any code you write here will run when the form opens.
+    
+    chartid = 97
+    chart_copy = app_tables.charts.get(id = chartid) 
+    print(chart_copy['id'])
+    chart_position = 11
     create_step_chart(self,chart_copy, chart_position)
     # Any code you write here will run when the form opens.    
 
